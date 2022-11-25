5bb52a7b8377af466ff3c4362ffe0ce2e1697866
Sualeh Fatehi <sualeh@hotmail.com>
2022-11-24 19:06:20 -0500

    Add Trino plugin

---
+++ schemacrawler-trino/src/main/java/schemacrawler/tools/trino/TrinoDatabaseConnector.java
@@ -0,0 +1,69 @@
+/*
+========================================================================
+SchemaCrawler
+http://www.schemacrawler.com
+Copyright (c) 2000-2022, Sualeh Fatehi <sualeh@hotmail.com>.
+All rights reserved.
+------------------------------------------------------------------------
+
+SchemaCrawler is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+SchemaCrawler and the accompanying materials are made available under
+the terms of the Eclipse Public License v1.0, GNU General Public License
+v3 or GNU Lesser General Public License v3.
+
+You may elect to redistribute this code under any of these licenses.
+
+The Eclipse Public License is available at:
+http://www.eclipse.org/legal/epl-v10.html
+
+The GNU General Public License v3 and the GNU Lesser General Public
+License v3 are available at:
+http://www.gnu.org/licenses/
+
+========================================================================
+*/
+
+package schemacrawler.tools.trino;
+
+import static schemacrawler.schemacrawler.MetadataRetrievalStrategy.none;
+import static schemacrawler.schemacrawler.SchemaInfoMetadataRetrievalStrategy.indexesRetrievalStrategy;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import schemacrawler.schemacrawler.DatabaseServerType;
+import schemacrawler.tools.databaseconnector.DatabaseConnector;
+import schemacrawler.tools.executable.commandline.PluginCommand;
+import us.fatehi.utility.datasource.DatabaseConnectionSourceBuilder;
+
+public final class TrinoDatabaseConnector extends DatabaseConnector {
+
+  private static final Logger LOGGER = Logger.getLogger(TrinoDatabaseConnector.class.getName());
+
+  public TrinoDatabaseConnector() {
+    super(
+        new DatabaseServerType("trino", "Trino"),
+        url -> url != null && url.startsWith("jdbc:trino:"),
+        (informationSchemaViewsBuilder, connection) -> {},
+        (schemaRetrievalOptionsBuilder, connection) ->
+            schemaRetrievalOptionsBuilder.with(indexesRetrievalStrategy, none),
+        limitOptionsBuilder -> {},
+        () ->
+            DatabaseConnectionSourceBuilder.builder("jdbc:trino://${host}:${port}/${database}")
+                .withDefaultPort(8080));
+    LOGGER.log(Level.INFO, "Loaded commandline for Trino");
+  }
+
+  @Override
+  public PluginCommand getHelpCommand() {
+    final PluginCommand pluginCommand = super.getHelpCommand();
+    pluginCommand
+        .addOption(
+            "server", String.class, "--server=trino%n" + "Loads SchemaCrawler plug-in for Trino")
+        .addOption("database", String.class, "Database name");
+    return pluginCommand;
+  }
+}
---
+++ schemacrawler-trino/src/test/java/schemacrawler/integration/test/TrinoTest.java
@@ -0,0 +1,94 @@
+/*
+========================================================================
+SchemaCrawler
+http://www.schemacrawler.com
+Copyright (c) 2000-2022, Sualeh Fatehi <sualeh@hotmail.com>.
+All rights reserved.
+------------------------------------------------------------------------
+
+SchemaCrawler is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+SchemaCrawler and the accompanying materials are made available under
+the terms of the Eclipse Public License v1.0, GNU General Public License
+v3 or GNU Lesser General Public License v3.
+
+You may elect to redistribute this code under any of these licenses.
+
+The Eclipse Public License is available at:
+http://www.eclipse.org/legal/epl-v10.html
+
+The GNU General Public License v3 and the GNU Lesser General Public
+License v3 are available at:
+http://www.gnu.org/licenses/
+
+========================================================================
+*/
+package schemacrawler.integration.test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+import static schemacrawler.test.utility.ExecutableTestUtility.executableExecution;
+import static schemacrawler.test.utility.FileHasContent.classpathResource;
+import static schemacrawler.test.utility.FileHasContent.hasSameContentAs;
+import static schemacrawler.test.utility.FileHasContent.outputOf;
+import static schemacrawler.test.utility.TestUtility.javaVersion;
+
+import java.util.regex.Pattern;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.TrinoContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import schemacrawler.schemacrawler.LimitOptionsBuilder;
+import schemacrawler.schemacrawler.SchemaCrawlerOptions;
+import schemacrawler.schemacrawler.SchemaCrawlerOptionsBuilder;
+import schemacrawler.test.utility.BaseAdditionalDatabaseTest;
+import schemacrawler.tools.executable.SchemaCrawlerExecutable;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@Testcontainers
+public class TrinoTest extends BaseAdditionalDatabaseTest {
+
+  private final DockerImageName imageName = DockerImageName.parse("trinodb/trino");
+
+  @Container
+  private final JdbcDatabaseContainer<?> dbContainer = new TrinoContainer(imageName.withTag("403"));
+
+  @BeforeEach
+  public void createDatabase() {
+
+    if (!dbContainer.isRunning()) {
+      fail("Testcontainer for database is not available");
+    }
+
+    createDataSource(
+        dbContainer.getJdbcUrl(), dbContainer.getUsername(), dbContainer.getPassword());
+  }
+
+  @Test
+  public void testTrinoWithConnection() throws Exception {
+
+    final LimitOptionsBuilder limitOptionsBuilder =
+        LimitOptionsBuilder.builder().includeSchemas(Pattern.compile("tpch\\.sf100"));
+    final SchemaCrawlerOptions schemaCrawlerOptions =
+        SchemaCrawlerOptionsBuilder.newSchemaCrawlerOptions()
+            .withLimitOptions(limitOptionsBuilder.toOptions());
+    schemaCrawlerOptions.withLimitOptions(limitOptionsBuilder.toOptions());
+
+    final SchemaCrawlerExecutable executable = new SchemaCrawlerExecutable("details");
+    executable.setSchemaCrawlerOptions(schemaCrawlerOptions);
+
+    final String expectedResource = String.format("testTrinoWithConnection.%s.txt", javaVersion());
+    assertThat(
+        outputOf(executableExecution(getDataSource(), executable)),
+        hasSameContentAs(classpathResource(expectedResource)));
+  }
+}
